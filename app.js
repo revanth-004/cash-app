637b1a85496182f8c352015ca474456735c94b12
Revanth kumar S <revanthsureshlearn@gmail.com>
2024-05-30 02:03:56 +0530

    mobile-responsive

---
+++ app.js
@@ -0,0 +1,9 @@
+function openNav() {
+    
+  document.getElementById("navbar-mobile").style.width = "250px";
+
+}
+
+function closeNav() {
+  document.getElementById("navbar-mobile").style.width = "0";
+}
