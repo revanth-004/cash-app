d6ece5766b96f9c2ae97f402439e7c2e9cd06086
Revanth kumar S <revanthsureshlearn@gmail.com>
2024-05-30 02:35:38 +0530

    GSAP

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 function openNav() {
-    
   document.getElementById("navbar-mobile").style.width = "250px";
 
 }
@@ -7,3 +6,37 @@ function openNav() {
 function closeNav() {
   document.getElementById("navbar-mobile").style.width = "0";
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(TextPlugin)
+  
+ });
+
+document.querySelectorAll('.floating-animation').forEach((element) => {
+ const randomDelay = Math.random() * 10;
+
+ gsap.to(element, {
+    rotate:5,
+    y: -10,
+    repeat: -1, 
+    yoyo: true, 
+    ease: "power1.inOut",
+    duration: 4 ,
+    delay:randomDelay
+        });
+});
+
+
+document.querySelectorAll('.boost-animation').forEach((element) => {
+ const randomDelay = Math.random() * 10;
+
+ gsap.to(element, {
+    rotate:2,
+    y: -5,
+    repeat: -1, 
+    yoyo: true, 
+    ease: "power1.inOut",
+    duration: 4 ,
+    delay:randomDelay
+        });
+});
